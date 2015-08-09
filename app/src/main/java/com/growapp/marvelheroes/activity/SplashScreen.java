186621f1055548c695466eee9db7a5ab553d002c
AlexKorRnd <AlexKorRnd@yandex.ru>
2015-08-09 15:11:31 +0300

    сделан сплеш скрин, загрузка из БД при отсутствии интернета

---
+++ app/src/main/java/com/growapp/marvelheroes/activity/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.growapp.marvelheroes.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.growapp.marvelheroes.R;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread logoTimer = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    int logoTimer = 0;
+                    while(logoTimer < 3000)
+                    {
+                        sleep(100);
+                        logoTimer = logoTimer +100;
+                    }
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    finish();
+                }
+            }
+        };
+        logoTimer.start();
+    }
+
+}
